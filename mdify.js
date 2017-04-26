c7cb52eae3c2c0e0bdc9b68920cb685d8a53e162
Stefan Stölzle <stoe@github.com>
2017-04-26 07:33:24 +0200

    Prepare it

--- mdify.js
+++ mdify.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const Promise = require('bluebird');
 const chalk = require('chalk');
-const mammoth = require('mammoth');
-const md = require('to-markdown');
 const opn = require('opn');
 
 Promise.promisifyAll(fs); // eslint-disable-line no-use-extend-native/no-use-extend-native
@@ -17,44 +15,13 @@ module.exports = class MDify {
 
   makeHTML() {
     return new Promise((resolve, reject) => {
-      mammoth
-        .convertToHtml({path: this.options.source})
-        .then(result => {
-          if (this.options.debug) {
-            fs.writeFileAsync(this.options.debug, result.value);
-          }
-
-          resolve(result.value);
-        })
-        .catch(err => {
-          reject(err);
-        });
+      // TODO
     });
   }
 
   makeMD(html, ora) {
     return new Promise((resolve, reject) => {
-      try {
-        const destination = path.resolve(this.options.destination);
-        const markdown = md(html, {gfm: true});
-
-        fs.writeFileAsync(this.options.destination, markdown).then(() => {
-          resolve(markdown);
-        });
-
-        if (this.options.open) {
-          opn(destination, {
-            wait: false
-          });
-
-          ora.info(
-            `opening ${chalk.blue(this.options.destination)} once created`
-          );
-        }
-      } catch (err) {
-        console.log(err);
-        reject(err);
-      }
+      // TODO
     });
   }
 };
